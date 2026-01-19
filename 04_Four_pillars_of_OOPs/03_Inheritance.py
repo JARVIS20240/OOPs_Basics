fee072ab8085ace1175b059da1bdf7d322a33e11
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-19 21:36:14 +0530

    End of today with learning OOPs Fundamentals

--- 04_Four_pillars_of_OOPs/03_Inheritance.py
+++ 04_Four_pillars_of_OOPs/03_Inheritance.py
@@ -1,3 +1,41 @@
 # Inheritance :
 '''one class uses the properties and methods of another class.'''
 
+# Without Inheritance:
+class parent:
+    @staticmethod
+    def hello():
+        print("Hello.!")
+    
+    @staticmethod
+    def welcome():
+        print("welcome.!")
+
+class child :
+    def __init__(self, name):
+        self.name = name
+
+ch1 = child("jhon")
+print(ch1.name)
+# print(ch1.hello()) #This Gives Error Bec... 
+
+# With Inheritance:
+class Cars: # Parent Class
+    @staticmethod
+    def start():
+        return "Car Started..."
+    
+    @staticmethod
+    def stop():
+        return "Car Stopped..."
+
+class ToyotaCar(Cars): # ToyotaCar is a Child Class of Parent class (Cars)
+    def __init__(self, name):
+        self.name = name
+
+car1 = ToyotaCar("Fortuner")
+car2 = ToyotaCar("Prius")
+
+print(car1.name)
+print(car1.start()) #If there is No inheritance we do there is an error
+print(car1.stop())
