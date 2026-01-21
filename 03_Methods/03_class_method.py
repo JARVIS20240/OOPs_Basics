36d8f4ae0ff57fea52601b94be82e4d16676fdf2
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-21 22:51:26 +0530

    class method

--- 03_Methods/03_class_method.py
+++ 03_Methods/03_class_method.py
@@ -1,9 +1,49 @@
+"""
+Class Method :
+with the help of class method we can read or modify class level data that is shared by all object.
+"""
+# Normal way to change Class Attribute.
+# Not change:
 class person:
     name = "Unknown"
 
-    def __init__(self, name):
+    def changename(self, name):
         self.name = name
 
-p1 = person('karan')
+p1 = person()
+p1.changename("karan")
 print(p1.name)
 print(person.name)
+
+# Change:
+class user :
+    name = "Unknbown"
+
+    def changeN(self, name):
+        user.name = name
+        # self.__class__.name = name
+u1 = user()
+u1.changeN("karan")
+print(u1.name)
+print(user.name)
+
+# change with Class method:
+"""
+Syntex:
+    class person:
+        @classmethod
+        def changeName(cls):
+            pass
+"""
+class user_data:
+    name = "Not_defined"
+
+    @classmethod
+    def chnage(cls, name):
+        cls.name=name
+
+ud1 = user_data()
+print(ud1.name)
+ud1.chnage("Karan_Mistr") #Chnage Not_Defined Class Attribute Value
+print(ud1.name)
+print(user_data.name)
