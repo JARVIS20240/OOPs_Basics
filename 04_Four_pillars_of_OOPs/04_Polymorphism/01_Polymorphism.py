7ba1155ec9f1fca0ec28066d9f8e5fa3a77fc1d5
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-22 19:32:21 +0530

    add dunder method

--- 04_Four_pillars_of_OOPs/04_Polymorphism/01_Polymorphism.py
+++ 04_Four_pillars_of_OOPs/04_Polymorphism/01_Polymorphism.py
@@ -23,12 +23,12 @@ class Complex:
     def showNumber(self):
         print(self.real,"i +", self.img,"j")
         
-    def _add_(self, num2):
+    def __add__(self, num2):
         newReal = self.real + num2. real
         newImg = self.img + num2.img
         return Complex (newReal, newImg)
     
-    def _sub_(self, num2):
+    def __sub__(self, num2):
         newReal = self. real - num2. real
         newImg = self.img - num2.img
         return Complex (newReal, newImg)
@@ -38,3 +38,6 @@ num1.showNumber()
 
 num2 = Complex (4, 6)
 num2.showNumber()
+
+num3 = num1 + num2
+num3.showNumber()
