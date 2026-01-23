8cded442686788464aadcd954af9eb042d6c3c19
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-23 19:52:04 +0530

    Revision-1/ Code with Hearry

--- 01_Basics_Class_Object/03_Instance_vs_Class_Attrib.py
+++ 01_Basics_Class_Object/03_Instance_vs_Class_Attrib.py
@@ -6,6 +6,8 @@ e1 = Employee()
 e2 = Employee()
 print(f"{e1.language}\n{e2.language}")
 e2.language = "JavaScript"
+
 print("\nInstance Attribute taks prefrances over the Class Attributes during the assignment and retrival.")
 print("\n(Instance Attributes > Class Attributes):")
+
 print(f"{e1.language}\n{e2.language}")
